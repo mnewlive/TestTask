94964f9952b1fcbc2580612b16ca4470f1bde632
Vadim Morozov <morozovvadim91@gmail.com>
2016-08-17 20:45:10 +0300

    Add FacebookSDK lib, permission, login button

---
+++ AndroidStudioProjects/TestTask/app/src/main/java/com/simpals/testtask/Country.java
@@ -0,0 +1,13 @@
+package com.simpals.testtask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Country extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country);
+    }
+}
--- AndroidStudioProjects/TestTask/app/src/main/java/com/simpals/testtask/MainActivity.java
+++ AndroidStudioProjects/TestTask/app/src/main/java/com/simpals/testtask/MainActivity.java
@@ -1,13 +1,59 @@
 package com.simpals.testtask;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String USER_FRIENDS = "user_friends";
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(getApplication());
+        callbackManager = CallbackManager.Factory.create();
+
         setContentView(R.layout.activity_main);
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(USER_FRIENDS);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                startActivity(new Intent(getApplicationContext(),Country.class));
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), R.string.on_cancel_message, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Toast.makeText(getApplicationContext(), R.string.on_error_message, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
